296afae79b25440bab6d000ca4f5df09307da631
Cesar2k94 <juliomederosarias@gmail.com>
2020-04-17 13:26:06 -0400

    Primer

---
+++ AspNETCore_StoredProcsDemo/Data/ValuesRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNETCore_StoredProcsDemo.Models;
+
+namespace AspNETCore_StoredProcsDemo.Data
+{
+    public class ValuesRepository
+    {
+        public Task<List<Value>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Value> GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert (Value value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteByID(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AspNETCore_StoredProcsDemo/Models/Value.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNETCore_StoredProcsDemo.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+    }
+}
